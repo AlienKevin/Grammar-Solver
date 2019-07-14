7fcf755d8156eaeeee1304c950c97b5e305479f5
Kevin Li <kevinli020508@gmail.com>
2019-07-14 16:35:24 -0700

    load input grammars into a Map

--- src/grammarsolver.cpp
+++ src/grammarsolver.cpp
@@ -1,11 +1,22 @@
-// This is the CPP file you will edit and turn in. (TODO: Remove this comment!)
-
 #include "grammarsolver.h"
+#include "map.h"
+#include "strlib.h"
+#include <iostream>
 using namespace std;
 
 Vector<string> grammarGenerate(istream& input, string symbol, int times) {
-    // TODO: write this function
-    
+    Map<string, Vector<string>> grammars;
+    string line;
+    while (getline(input, line)) {
+        string nonterminal = stringSplit(line, "::=")[0];
+        Vector<string> rules = stringSplit(stringSplit(line, "::=")[1], "|");
+        if (grammars.containsKey(nonterminal)) { // duplicated rules
+            throw "Error: Duplicated rules.";
+        } else {
+            grammars[nonterminal] = rules;
+        }
+    }
+    cout << grammars << endl;
     Vector<string> v;   // this is only here so it will compile
     return v;           // this is only here so it will compile
 }
